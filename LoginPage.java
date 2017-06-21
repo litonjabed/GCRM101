86971c5b6b92c15ac43d7c41915d41c52f178f90
litonjabed <jabedliton1@gmail.com>
2017-06-21 01:59:16 -0400

    Add files via upload

---
+++ LoginPage.java
@@ -0,0 +1,52 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import businessLib.BusinessLib;
+
+public class LoginPage extends BusinessLib
+{
+	
+	/*
+	 
+	 @Author: Test Engineer 1
+	 
+	 */
+	//here we are following a static method concept to main object properties
+	public static void staticmethod()
+	{
+		System.out.println("sample static method");
+	}
+	
+	
+	//user name object property
+	public static WebElement get_UserName_txtbox()
+	{
+		WebElement un=driver.findElement(By.id("uid"));
+		return un;
+	}
+	
+	//Password text box
+	public static WebElement get_password_txtbox()
+	{
+		WebElement pwd=driver.findElement(By.name("passw"));
+		return pwd;
+	}
+	
+	//Login Page
+	public static WebElement get_Login_btn()
+	{
+		WebElement login=driver.findElement(By.xpath("//input[@value='Login']"));
+		return login;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
